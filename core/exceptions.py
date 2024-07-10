006a90ba47b06a21c6d36400e1d118ef8619debf
Aghahowa Jeffrey <aghahowaj01@gmail.com>
2024-07-10 14:44:05 +0100

    Update exceptions.py

--- core/exceptions.py
+++ core/exceptions.py
@@ -1,6 +1,16 @@
 from rest_framework.views import exception_handler
 from rest_framework.response import Response
 
+
+class NoTokenError(exceptions.APIException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    default_detail = {
+        "status": "Bad request",
+        "message": "Authentication failed",
+        "statusCode": 401
+    }
+    default_code = 'no_token'
+
 def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
 
