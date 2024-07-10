66506b1e619122a49cc620eaab05adba3b789b99
Aghahowa Jeffrey <aghahowaj01@gmail.com>
2024-07-10 09:53:44 +0100

    fixed wsgi connections to settings

--- build_file.sh
+++ build_file.sh
@@ -11,8 +11,6 @@ python3 manage.py migrate --noinput
 
 echo "Creating dist folder..."
 mkdir -p dist
-
-echo "Copying necessary files to dist..."
-cp -r hng_stage2/wsgi.py dist/
+touch dist/dummy_file.py
 
 echo "Setup complete"
