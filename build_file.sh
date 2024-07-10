9ec3f2bf1ac09274efa2366a6a32c60d93db0029
Aghahowa Jeffrey <aghahowaj01@gmail.com>
2024-07-10 09:08:19 +0100

    updated build process to use pipenv

--- build_file.sh
+++ build_file.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-echo "Create a virtual environment"
-python3 -m venv venv
-source venv/Script/activate
+echo "Installing pipenv"
+python3 -m pip install pipenv
 
 echo "Building project packages..."
-python3 -m pip install -r requirements.txt
+python3 -m pipenv install --system --deploy --ignore-pipfile
 
 echo "Migrating Database..."
 python3 manage.py makemigrations --noinput
