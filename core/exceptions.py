1620901dda61309fa04064db28196b068bf3bf96
Aghahowa Jeffrey <aghahowaj01@gmail.com>
2024-07-10 14:07:03 +0100

    Create exceptions.py

---
+++ core/exceptions.py
@@ -0,0 +1,10 @@
+from rest_framework.views import exception_handler
+from rest_framework.response import Response
+
+def custom_exception_handler(exc, context):
+    response = exception_handler(exc, context)
+
+    if isinstance(exc, NoTokenError):
+        return Response(exc.default_detail, status=exc.status_code)
+
+    return response
