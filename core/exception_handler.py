217f1a19ac036d07eaeb28c2d787d1b14be2af9b
Aghahowa Jeffrey <aghahowaj01@gmail.com>
2024-07-10 15:34:28 +0100

    Update exception_handler.py

--- core/exception_handler.py
+++ core/exception_handler.py
@@ -1,1 +1,10 @@
 
+
+
+def custom_exception_handler(exc, context):
+    response = exception_handler(exc, context)
+
+    if isinstance(exc, NoTokenError):
+        return Response(exc.default_detail, status=exc.status_code)
+
+    return response
